c32aa73e3b410adfe8bf9d92954e4d47e0453d91
Omotayo Ogunrinde <omotayo@steer.global>
2026-08-27 11:08:21 -0400

    Switch backend auth to Azure AD (use_azuread_auth), fix listKeys 403

--- backend.tf
+++ backend.tf
@@ -4,5 +4,6 @@ terraform {
     storage_account_name = "omotayotfstate"
     container_name       = "tf-state"
     key                  = "landing-zone.tfstate"
+    use_azuread_auth     = true
   }
 }
